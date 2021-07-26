a143ac20d8dad400fdefc8de9d4e955c518089d4
Carlos Bedoy <carlos.bedoy@gmail.com>
2021-07-25 21:52:38 -0500

    Upgraded koin

--- app/src/main/java/com/cbedoy/rappimovie/MovieApp.kt
+++ app/src/main/java/com/cbedoy/rappimovie/MovieApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.cbedoy.core.di.coreModule
 import com.cbedoy.feature_moviedetail.di.featureMovieDetailModule
 import com.cbedoy.feature_movielist.di.featureMovieListModule
+import com.cbedoy.feature_moviesearch.di.featureMovieSearchModule
 import com.cbedoy.rappimovie.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -16,7 +17,9 @@ class MovieApp : Application() {
             modules(
                 appModule,
                 coreModule,
-                featureMovieListModule, featureMovieDetailModule
+                featureMovieListModule,
+                featureMovieDetailModule,
+                featureMovieSearchModule
             )
             androidContext(this@MovieApp)
         }
--- app/src/main/java/com/cbedoy/rappimovie/di/AppModule.kt
+++ app/src/main/java/com/cbedoy/rappimovie/di/AppModule.kt
@@ -1,11 +1,8 @@
 package com.cbedoy.rappimovie.di
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import org.koin.dsl.module
 
+
 val appModule = module {
-    factory {
-        CoroutineScope(Dispatchers.IO)
-    }
+
 }
